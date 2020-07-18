2b4fc5497710d4c2527d8f97d9c421355b90f0c2
Anıl Şenay <anilsenay3@gmail.com>
2020-07-18 14:58:10 +0300

    tailwindCss added

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p className="m-12">Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div className="test" style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <p> test</p>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
